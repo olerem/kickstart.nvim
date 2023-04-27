b1b56c63ccf4c79b477f5d307e9ae645f0b90c9f
Oleksij Rempel <linux@rempel-privat.de>
2023-04-27 11:11:37 +0200

    add clangd and vivien/vim-linux-coding-style

--- init.lua
+++ init.lua
@@ -73,6 +73,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- my persoanl plugins
+  'vivien/vim-linux-coding-style',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
@@ -405,7 +408,7 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
+     clangd = {},
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
